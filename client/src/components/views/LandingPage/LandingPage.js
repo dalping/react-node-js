f830023ea5f1bd8397e51dfbf0ee1f8a51856856
Dalping <kbing14@naver.com>
2021-09-10 17:34:42 +0900

    .

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,7 @@
-import axios from 'axios'
 import React from 'react'
 import {withRouter} from  'react-router-dom';
 
-
 function LandingPage(props) {
-
-
 
     return (
         <div style={{display:'flex', justifyContent:'center', alignItems:'center',width:'100%',height:'100vh'}}>
@@ -16,3 +12,4 @@ function LandingPage(props) {
 }
 
 export default withRouter(LandingPage)
+
